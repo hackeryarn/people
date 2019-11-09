f91183e709cac9bd8e36a93ea45538308777227f
Artem Chernyak <artem.chernyak@ocelotconsulting.com>
2019-11-09 13:56:21 -0600

    working parser

--- person/src/person/core.clj
+++ person/src/person/core.clj
@@ -1,12 +1,8 @@
 (ns person.core
-  (:require [clojure.string :as str]))
-
-(def psv "Artem|Chernyak|M|Green|05/03/1990")
-(def csv "Artem,Chernyak,M,Green,05/03/1990")
-(def ssv "Artem Chernyak M Green 05/03/1990")
+  (:require [clojure.string :as str]
+            [java-time :as time]))
 
 ;; Helpers
-
 (def gender-map
   "Possible user provided genders to consistent gender."
   {:m "m"
@@ -15,15 +11,21 @@
    :female "f"})
 
 (def valid-genders
-  ["m"
-   "f"
-   "male"
-   "female"])
+  "List of valid genders"
+  (map name (keys gender-map)))
 
 (defn gender?
   "Checks if string is a valid gender"
   [s]
   (some #{(str/lower-case s)} valid-genders))
+
+(defn date-str?
+  "Checks for valid date string"
+  [date]
+  (boolean (re-matches #"\d\d\/\d\d\/\d\d\d\d" date)))
+
+;; Parser
+(defrecord Person [last-name first-name gender favorite-color date-of-birth])
 
 (defn split-str
   "Splits a string by '|', ',', or ' '."
@@ -34,10 +36,6 @@
     (str/includes? s " ") (str/split s #"\ ")
     :else []))
 
-;; Person functions
-
-(defrecord Person [last-name first-name gender favorite-color date-of-birth])
-
 (defn split-str->person
   "Creates a person from a split string"
   [[last-name first-name gender favorite-color date-of-birth]]
@@ -45,14 +43,17 @@
          (string? first-name)
          (gender? gender)
          (string? favorite-color)
-         (string? date-of-birth)]}
-  (->Person last-name first-name gender favorite-color date-of-birth))
+         (date-str? date-of-birth)]}
+  (->Person
+   last-name
+   first-name
+   ((keyword (str/lower-case gender)) gender-map)
+   favorite-color
+   (time/local-date "MM/dd/yyyy" date-of-birth)))
 
-(defn string->person
+(defn str->person
   "Parses a string separated by '|', ',', or ' ' into a person record"
   [s]
   (-> s
       split-str
       split-str->person))
-
-(string->person psv)
--- person/test/person/core_test.clj
+++ person/test/person/core_test.clj
@@ -1,7 +1,33 @@
 (ns person.core-test
   (:require [clojure.test :refer :all]
-            [person.core :refer :all]))
+            [java-time :as time]
+            [person.core :as p]))
 
-(deftest split-str
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest str->person
+  (def valid-psv "Chernyak|Artem|Male|Green|05/03/1990")
+  (def valid-csv "Chernyak,Artem,M,Green,05/03/1990")
+  (def valid-ssv "Chernyak Artem M Green 05/03/1990")
+
+  (def valid-person
+    (p/map->Person {:first-name "Artem"
+                    :last-name "Chernyak"
+                    :gender "m"
+                    :favorite-color "Green"
+                    :date-of-birth (time/local-date
+                                    "MM/dd/yyyy"
+                                    "05/03/1990")}))
+
+  (testing "pipe separated values"
+    (is (= valid-person (p/str->person valid-psv))))
+  (testing "comma separated values"
+    (is (= valid-person (p/str->person valid-csv))))
+  (testing "space separated values"
+    (is (= valid-person (p/str->person valid-ssv))))
+
+  (def no-separator "ChernyakArtem")
+
+  (testing "missing separators"
+    (try
+      (is (thrown? AssertionError (p/str->person no-separator))))))
+
+(run-tests)
