493bc262ecb0204db0d5d98089d25198bb065c6b
Artem Chernyak <hackeryarn@pop-os.localdomain>
2019-11-09 12:37:31 -0600

    moved to using a person record

--- person/src/person/core.clj
+++ person/src/person/core.clj
@@ -5,6 +5,26 @@
 (def csv "Artem,Chernyak,M,Green,05/03/1990")
 (def ssv "Artem Chernyak M Green 05/03/1990")
 
+;; Helpers
+
+(def gender-map
+  "Possible user provided genders to consistent gender."
+  {:m "m"
+   :f "f"
+   :male "m"
+   :female "f"})
+
+(def valid-genders
+  ["m"
+   "f"
+   "male"
+   "female"])
+
+(defn gender?
+  "Checks if string is a valid gender"
+  [s]
+  (some #{(str/lower-case s)} valid-genders))
+
 (defn split-str
   "Splits a string by '|', ',', or ' '."
   [s]
@@ -14,5 +34,25 @@
     (str/includes? s " ") (str/split s #"\ ")
     :else []))
 
-;; Parse date into a map
-;; Create specs for all functions
+;; Person functions
+
+(defrecord Person [last-name first-name gender favorite-color date-of-birth])
+
+(defn split-str->person
+  "Creates a person from a split string"
+  [[last-name first-name gender favorite-color date-of-birth]]
+  {:pre [(string? last-name)
+         (string? first-name)
+         (gender? gender)
+         (string? favorite-color)
+         (string? date-of-birth)]}
+  (->Person last-name first-name gender favorite-color date-of-birth))
+
+(defn string->person
+  "Parses a string separated by '|', ',', or ' ' into a person record"
+  [s]
+  (-> s
+      split-str
+      split-str->person))
+
+(string->person psv)
