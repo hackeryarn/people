27a7699622b1b216435570443324dc0eb18479ff
Artem Chernyak <hackeryarn@pop-os.localdomain>
2019-11-13 21:35:37 -0600

    removed extra dependency

--- person/project.clj
+++ person/project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [person "0.1.0-SNAPSHOT"]
                  [clojure.java-time "0.3.2"]]
   :plugins [[lein-cloverage "1.1.2"]]
   :repl-options {:init-ns person.core})
